5be7dfb6f69e90400721d07b8034bb10bf0b22d4
ilopezl05 <66643661+ilopezl05@users.noreply.github.com>
2022-04-02 03:05:18 -0500

    Added limited chance to guess word and some messages for the user.

--- wordle.py
+++ wordle.py
@@ -27,46 +27,67 @@ def main():
 
     lettersNotInWord = []
     rightWord = False
+    tries = 0
 
     while rightWord != True:
-        # Get try word from user
-        tryWord = input('\nEnter your word: ')
-        # Check if try word is 5 characters long
-        while len(tryWord) != 5:
-            print('Make sure you are entering a 5 letter word.')
+
+        if tries <= 5:
+            # FYI to warn the user of final opportunity
+            if tries == 5:
+                print('\n--------This is your last chance, make it count.--------')
+
+            # Get try word from user
             tryWord = input('\nEnter your word: ')
+            # Check if try word is 5 characters long
+            while len(tryWord) != 5:
+                print('--------Make sure you are entering a 5 letter word.--------')
+                tryWord = input('\nEnter your word: ')
 
-        # Find letters in right place
-        for tryLetter, guessLetter in zip(tryWord, wordToGuess):
-            if tryLetter == guessLetter:
-                print('"{}" is in the right place.'.format(tryLetter))
-            else:
-                # Find letters not in right place
-                if tryLetter in wordToGuess:
-                    print('"{}" is in the word, but not in the right place.'.format(tryLetter))
+            # Increase tries by one very time a word is submitted
+            tries += 1
+
+            # Find letters in right place
+            for tryLetter, guessLetter in zip(tryWord, wordToGuess):
+                if tryLetter == guessLetter:
+                    print('"{}" is in the right place.'.format(tryLetter))
                 else:
-                    # Add character to list of letters not in the word to be guessed
-                    if tryLetter not in lettersNotInWord:
-                        lettersNotInWord.append(tryLetter)
+                    # Find letters not in right place
+                    if tryLetter in wordToGuess:
+                        print('"{}" is NOT in the right place.'.format(tryLetter))
+                    else:
+                        # Add character to list of letters not in the word to be guessed
+                        if tryLetter not in lettersNotInWord:
+                            lettersNotInWord.append(tryLetter)
 
-        print('\nIncorrect letters:')
-        print(lettersNotInWord)
+            print('\nIncorrect letters:')
+            lettersNotInWord.sort()
+            print(lettersNotInWord)
 
-        # check if user guessed the word
-        if tryWord == wordToGuess:
-            print('Good job, the word was {}'.format(wordToGuess))
-            keepPlaying = input('\nDo you want to continue playing (y for yes, anything else for no)? ')
+            # check if user guessed the word
+            if tryWord == wordToGuess:
+                print('\n--------Good job, the word was {}--------'.format(wordToGuess))
+                rightWord, tries, lettersNotInWord, wordToGuess = continuePlaying(rightWord, tries, lettersNotInWord, words)
 
-            if keepPlaying == 'Y' or keepPlaying == 'y':
-                main()
-            else:
-                print('\nSee you around.')
-
-            rightWord = True
+        else:
+            print("\n--------Good game, you'll get it next time.--------")
+            rightWord, tries, lettersNotInWord, wordToGuess = continuePlaying(rightWord, tries, lettersNotInWord, words)
 
 def getWord(list):
     randNum = random.randrange(0, len(list))
     return list[randNum]
+
+def continuePlaying(breaker, counter, letterList, words):
+    keepPlaying = input('\nDo you want to continue playing (y for yes, anything else for no)? ')
+    if keepPlaying == 'Y' or keepPlaying == 'y':
+        word = getWord(words)
+        breaker = False
+        counter = 0
+        letterList = []
+        return breaker, counter, letterList, word
+    else:
+        print('\nSee you around.')
+        breaker = True
+        return breaker, counter,letterList, words
 
 if __name__ == '__main__':
     main()
